c26b4f3eeebe47271e2c42125f6c8d9c2007f757
Benjamin Jensen <ben@tamam.dk>
2022-04-14 15:33:29 +0200

    Removed lvgl_esp32_drivers as it is obsolite

---
+++ main/include/gui.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/*******************************************************
+ *                Macros
+ *******************************************************/
+
+
+/*******************************************************
+ *                Type Definitions
+ *******************************************************/
+
+
+/*******************************************************
+ *                Function Declarations
+ *******************************************************/
+
+/**
+ * @brief Updates the button state machines
+ *         The debounce state machine has 4 calls as the timing
+ *          meaning than for every 4th call with same value state changes
+ * @param but bitmask of current sampled button state
+ * @return ESP_OK on success
+ */
+
